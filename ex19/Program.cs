47172016a96dd7c39f473b6ca6c0c924476c21a4
Yuri E <196240@gmail.com>
2022-11-30 10:13:31 +0100

    initial commit

---
+++ ex19/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter a number");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N > 99) {
+
+    int M = N%1000 / 100;
+    Console.WriteLine (M);
+
+}
+
+else {
+
+    Console.WriteLine ("There are no 3rd digit");
+}
