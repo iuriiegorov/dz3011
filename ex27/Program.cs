54f20226f1dc1f8fa8fe5fb103881830e5f37296
Yuri E <196240@gmail.com>
2022-12-04 11:40:05 +0100

    ex27 added

---
+++ ex27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("Enter a number");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+
+while ( n > 0 ) {
+    
+    sum = sum + n % 10;
+    n = n / 10;
+    
+}
+
+Console.WriteLine(sum);
