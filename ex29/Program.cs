32d5c3ce95ade6ce249fc1e90b19f344bf95fec0
Yuri E <196240@gmail.com>
2022-12-04 12:19:40 +0100

    ex29 added

---
+++ ex29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int [] array = new int[8];
+int index = 0;
+while (index < 8 )
+{
+    array[index] = new Random().Next(1, 100);
+    Console.WriteLine(array[index]);
+    index++;
+}
