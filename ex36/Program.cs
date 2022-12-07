664c263f7307e4ced4b2b0dcfa76204d521a7d7e
Yuri E <196240@gmail.com>
2022-12-07 11:41:11 +0100

    ex36 added

---
+++ ex36/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int [] array = new int[4];
+int index = 0;
+int sum = 0;
+while (index < 4 ) {
+    array[index] = new Random().Next(-100, 100);
+
+      if (index%2 > 0) {
+            sum = sum + array[index];
+        };
+        
+    Console.WriteLine(array[index]);
+    index++;
+}
+Console.WriteLine("Sum of odd numbers = " + sum);
