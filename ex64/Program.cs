972191acd7247abd6f6e0f9ccc45545bacfabc27
Yuri E <196240@gmail.com>
2022-12-23 14:44:37 +0100

    last commit

---
+++ ex64/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+Console.WriteLine("Enter a number");
+int n = Convert.ToInt32(Console.ReadLine());
+int num(int n1) {
+
+    Console.WriteLine($"{(n1)} ");
+    if (n1 == 1) return n1;
+    return num(n1-1);
+    
+}
+
+num(n);
---
+++ ex66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов
+//  в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Enter a min number");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter a max number");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+
+int num(int n1, int m1) {
+
+    sum =sum + n1;
+    if (m1 == n1) return n1;
+    return num(n1+1, m1);
+    
+}
+
+num(n,m);
+Console.WriteLine(sum);
